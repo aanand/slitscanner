13595bbd8583c5b2e2c10ac5a105107584bc7191
Aanand Prasad <aanand.prasad@gmail.com>
2016-04-16 12:12:17 +0100

    Set logging level to INFO

--- bot.py
+++ bot.py
@@ -220,11 +220,11 @@ class SlitScanner(TwitterBot):
 
 def start_logging():
     stderr = logging.StreamHandler()
-    stderr.setLevel(logging.DEBUG)
+    stderr.setLevel(logging.INFO)
     stderr.setFormatter(logging.Formatter(fmt='%(levelname)8s: %(message)s'))
 
     root_logger = logging.getLogger()
-    root_logger.setLevel(logging.DEBUG)
+    root_logger.setLevel(logging.INFO)
     root_logger.addHandler(stderr)
 
 
