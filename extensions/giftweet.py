271b083d9916d0e5f926e181d7bb3dfccfc459b9
Aanand Prasad <aanand.prasad@gmail.com>
2016-04-20 20:51:24 +0100

    Remove scraping code - video url is in extended_entities

--- extensions/giftweet.py
+++ extensions/giftweet.py
@@ -1,8 +1,5 @@
 import logging
 import re
-import urllib
-
-from bs4 import BeautifulSoup
 
 
 log = logging.getLogger(__name__)
@@ -10,39 +7,11 @@ log = logging.getLogger(__name__)
 URL_PATTERN = re.compile(r'^https?://twitter\.com/(\w+)/status/(\d+)/photo/1$')
 
 
-def get_gif_video_url(api, tweet):
-    url = get_gif_page_url_climbing(api, tweet)
-    if url is None:
-        return None
-
-    tweet_id = URL_PATTERN.match(url).group(2)
-
-    log.info("Opening {}".format(url))
-    html = urllib.urlopen(url).read()
-    soup = BeautifulSoup(html)
-
-    tag = soup.find('div', attrs={"data-tweet-id": tweet_id})
-    if not tag:
-        log.info(
-            "Couldn't find a div with data-tweet-id={} - giving up"
-            .format(repr(tweet_id)))
-        return None
-
-    match = re.search(
-        r'https?://pbs\.twimg\.com/tweet_video_thumb/(.+)\.\w+', unicode(tag))
-
-    if not match:
-        log.info("Couldn't find a thumbnail URL - giving up")
-        return None
-
-    return "https://pbs.twimg.com/tweet_video/{}.mp4".format(match.group(1))
-
-
-def get_gif_page_url_climbing(api, tweet):
+def get_gif_video_url_climbing(api, tweet):
     me = api.me()
 
     while True:
-        url = get_gif_page_url(tweet)
+        url = get_gif_video_url(tweet)
 
         if url:
             return url
@@ -62,14 +31,19 @@ def get_gif_page_url_climbing(api, tweet):
         log.info("Climbing up to status {}".format(tweet_url(tweet)))
 
 
-# We're looking for a media entity with a "http://twitter.com/.../photo/1" URL
-def get_gif_page_url(tweet):
-    log.info("Getting GIF page URL for {}".format(tweet_url(tweet)))
+def get_gif_video_url(api, tweet):
+    log.info("Getting GIF video URL for {}".format(tweet_url(tweet)))
 
-    for media in tweet.entities.get('media', []):
-        if URL_PATTERN.match(media['expanded_url']):
-            log.info("Found link: {}".format(media['expanded_url']))
-            return media['expanded_url']
+    if not hasattr(tweet, 'extended_entities'):
+        log.info("Tweet has no extended_entities attribute - giving up")
+        return None
+
+    extended_entities = tweet.extended_entities
+    log.debug("Extended entities: {}".format(extended_entities))
+
+    for media in extended_entities.get('media', []):
+        if media.get('type') == 'animated_gif':
+            return media['video_info']['variants'][0]['url']
 
     log.info("Tweet has no media entity with a matching URL - giving up")
     return None
