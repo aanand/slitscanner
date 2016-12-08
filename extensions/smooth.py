2f38abc4d42f47d7a555d0718a6b5319a3f72150
Aanand Prasad <aanand.prasad@gmail.com>
2016-12-08 12:25:55 +0000

    Skip smoothing step if butterflow can't be found

--- extensions/smooth.py
+++ extensions/smooth.py
@@ -1,12 +1,20 @@
 import logging
 
 from botutils.command import check_call
+from botutils.command import CalledProcessError
 
 
 log = logging.getLogger(__name__)
 
 
 def smooth_video(filename, destination, rate):
+    try:
+        check_call(['which', 'butterflow'])
+    except CalledProcessError:
+        log.warn('butterflow not found - skipping smoothing step')
+        check_call(['cp', filename, destination])
+        return
+
     output = check_call([
         'butterflow',
         '-v',   # verbose
